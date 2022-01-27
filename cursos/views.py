cca5a377b2efa28f20c9f152a1dd99ee765012a5
coabr <carol.o.ag@gmail.com>
2022-01-26 16:39:19 -0800

    Cria app cursos

---
+++ cursos/views.py
@@ -0,0 +1,24 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    """
+    API REST para cursos
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    """
+    API REST para avaliações
+    """
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
