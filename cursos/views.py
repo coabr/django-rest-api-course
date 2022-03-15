bacf3863e950664df97bad18cdfc66892d11010d
coabr <carol.o.ag@gmail.com>
2022-03-15 11:10:23 -0700

    Customizando as views com mixins

--- cursos/views.py
+++ cursos/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.decorators import action # muda açoes dentro do nosso model view
 from rest_framework.response import Response
+from rest_framework import mixins
 
 
 from .models import Curso, Avaliacao
@@ -61,7 +62,22 @@ class CursoViewSet(viewsets.ModelViewSet):
         serializers = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
         return Response(serializers.data)
 
-
+"""
 class AvaliacaoViewSet(viewsets.ModelViewSet):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+"""
+
+class AvaliacaoViewSet(mixins.ListModelMixin,
+                       mixins.CreateModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.DestroyModelMixin,
+                       viewsets.GenericViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()  
